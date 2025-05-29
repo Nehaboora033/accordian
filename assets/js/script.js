202f87837c24b6b67708c48a85eaa70bfc8cf731
Neha <neha@Nehas-MacBook-Air.local>
2025-05-29 14:02:20 +0530

    added arrow

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function toggleAccordion(selector) {
+    const clickedItem = document.querySelector(selector);
+    const allItems = document.querySelectorAll('.accordion-item');
+
+    allItems.forEach(item => {
+        if (item !== clickedItem) {
+            item.classList.remove('active');
+        }
+    });
+
+    clickedItem.classList.toggle('active');
+}
